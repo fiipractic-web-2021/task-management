d261fec43e1b364e166541b476b70bf0ef905427
Andrei Luca <andrei.x.luca@kp.org>
2021-03-24 22:46:02 +0200

    update code for week 4

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,41 +1,46 @@
-/**
- * Exercitii:
- *
- * Basic
- * 1. adaugati css sa formatati mai bine formularul
- * 2. schimbati locatia in document a formularului
- * 3. stergeti formularul si evenimentele asociate dupa ce adaugam task-ul
- *  HINT: refactor the submit handler
- *
- * Advanced
- * 3. creati un modal simplu pentru form, cu sau fara backdrop
- * 4. adaugati posibilitatea de schimbare a coloanei unde va fi adaugat taskul
- * 5. creati un array de task-uri si actualizati scriptul sa afiseze task-uri din acel array
- */
-
 const taskTemplate = `
-<div class="task">
-<p class="task-title">{title}</p>
-<div class="task-details">
-  <div class="task-info">
-    <div class="avatar"></div>
-    <p class="task-code">
-      {tag}
-    </p>
-  </div>
-  <div class="task-status">
-    <div class="task-type">
-      <i class="fa fa-bookmark"></i>
+  <div class="task">
+    <p class="task-title">{title}</p>
+    <div class="task-details">
+      <div class="task-info">
+        <div class="avatar"></div>
+        <p class="task-code">
+          {tag}
+        </p>
+      </div>
+      <div class="task-status">
+        <div class="task-type">
+          <i class="fa {taskType}"></i>
+        </div>
+        <div class="task-priority">
+          <i class="fa fa-arrow-up {priority}"></i>
+        </div>
+      </div>
     </div>
-    <div class="task-priority">
-      <i class="fa fa-arrow-up"></i>
-    </div>
-  </div>
-</div>
-</div>`;
+  </div>`;
+
+const taskTypeIcons = {
+  'task': 'fa-bookmark icon-blue',
+  'improvement': 'fa-chart-line icon-teal',
+  'bug': 'fa-bug icon-red'
+};
+
+const priorityIcons = {
+  'low': 'icon-green',
+  'medium': 'icon-yellow',
+  'high': 'icon-orange',
+  'urgent': 'icon-red',
+};
+
+const tasks = {
+  backlog: [],
+  selected: [],
+  inProgress: [],
+  done: []
+};
 
 const backlog = document.querySelector(".board section:first-child .tasks");
-const addTaskButton = document.getElementById("addTask");
+const addTaskButton = document.getElementById("addTask").addEventListener("click", showForm);
 
 function compileToNode(domString) {
   const div = document.createElement("div");
@@ -44,53 +49,119 @@ function compileToNode(domString) {
   return div.firstElementChild;
 }
 
-function compileTaskTemplate(title, tag, template) {
+function compileTaskTemplate(title, tag, taskType, priority, template) {
   const compiledTemplate = template
     .replace("{title}", title)
-    .replace("{tag}", tag);
+    .replace("{tag}", tag)
+    .replace("{taskType}", getTaskTypeIcon(taskType))
+    .replace("{priority}", getPriorityIcon(priority));
   return compileToNode(compiledTemplate);
 }
 
-function addTask(title, tag) {
-  const task = compileTaskTemplate(title, tag, taskTemplate);
+function addTask(title, taskType, priority) {
+  const newTask = {
+    title: title,
+    taskType: taskType,
+    priority: priority,
+    tag: getId(taskType)
+  }
+  tasks.backlog.push(newTask);
+  const task = compileTaskTemplate(newTask.title, newTask.tag, newTask.taskType, newTask.priority, taskTemplate);
   backlog.appendChild(task);
-
-  // addTaskButton.removeEventListener("click", addTask);
 }
 
-addTaskButton.addEventListener("click", showForm);
 
 function showForm() {
-  const h1 = Array.from(document.getElementsByTagName('h1')).shift();
-  const form = h1.insertAdjacentElement("afterend", showAddForm());
+  const form = document.body.appendChild(showAddForm());
+  form.classList.add('show');
+  const closeButton = form.querySelector(".close");
 
-  form.addEventListener('submit', (event) => {
+  const closeAddTaskForm = () => {
+    form.removeEventListener('submit', submitTask);
+    closeButton.removeEventListener('click', closeAddTaskForm);
+    form.classList.remove('show');
+  }
+
+  const submitTask = (event) => {
     event.preventDefault();
-    const { target }  = event;
 
-    const title = target.querySelector('[name="title"]');
-    const tag = target.querySelector('[name="tag"]');
+    const { target } = event;
 
-    // todo validate data!!
-    addTask(title.value, tag.value);
-  });
+    const title = target.querySelector('[name="title"]').value;
+    const type = target.querySelector('[name="type"]').value;
+    const priority = target.querySelector('[name="priority"]').value;
+    addTask(title, type, priority);
+    closeAddTaskForm();
+  }
+
+  closeButton.addEventListener('click', closeAddTaskForm);
+  form.addEventListener('submit', submitTask);
 }
+
+function getId(taskType) {
+  const allTasks = Object.keys(tasks).reduce((accumulator, currentValue) => {
+    return accumulator += tasks[currentValue].length;
+  }, 0);
+  // the id for a new task will be based on the number of total tasks in the board
+  // with reduce, we get the total number of tasks
+  const taskNumber = allTasks + 1;
+  switch (taskType) {
+    case 'task':
+      return 'TASK-' + taskNumber;
+    case 'improvement':
+      return `IMPROVEMENT-${taskNumber}`;
+    default:
+      return 'BUG' + '-' + taskNumber;
+  }
+}
+
+const getTaskTypeIcon = taskType => {
+  const iconKeyValuePair = Object.entries(taskTypeIcons).find(([key, value]) => {
+    return key === taskType; // since it's a one-liner there's no need for {} and 'return' keyword
+  });
+  return iconKeyValuePair[1]; // index 0 is the key, index 1 is the value in which we are interested
+}
+
+const getPriorityIcon = priority => {
+  const iconKeyValuePair = Object.entries(priorityIcons).find(([key, value]) => {
+    return key === priority; // since it's a one-liner there's no need for {} and 'return' keyword
+  });
+  return iconKeyValuePair[1]; // index 0 is the key, index 1 is the value in which we are interested
+}
+
 
 function showAddForm() {
   const formString = `
-    <form id="addTaskForm" action="" method="POST">
-      <label for="title">Title</label>
-      <input type="text" name="title" id="title">
-      <label for="tag">Tag</label>
-      <select name="tag" id="tag">
-        <option value="low">LOW</option>
-        <option value="medium">MEDIUM</option>
-        <option value="high">HIGH</option>
-        <option value="urgent">URGENT</option>
-      </select>
-      <button name="submit" type="submit">Add task</button>
-    </form>
-  `.trim();
+    <div class="backdrop hide">
+      <div class="modal">
+        <h2 class="title">Add a new task</h2>
+        <i class="close fas fa-times fa-2x"></i>
+        <form id="addTaskForm" action="" method="POST">
+          <label for="title">Title</label>
+          <input type="text" name="title" id="title" required>
 
-    return compileToNode(formString);
+          <label for="type">Type</label>
+          <select name="type" id="type" required>
+            <option disabled selected value></option>
+            <option value="task">Task</option>
+            <option value="improvement">Improvement</option>
+            <option value="bug">Bug</option>
+          </select>
+
+
+          <label for="priority">Priority</label>
+          <select name="priority" id="priority" required>
+            <option disabled selected value></option>
+            <option value="low">LOW</option>
+            <option value="medium">MEDIUM</option>
+            <option value="high">HIGH</option>
+            <option value="urgent">URGENT</option>
+          </select>
+          <button class="btn-add" name="submit" type="submit">Add task</button>
+        </form>
+      </div>
+    </div>
+    `.trim();
+
+  return compileToNode(formString);
 }
